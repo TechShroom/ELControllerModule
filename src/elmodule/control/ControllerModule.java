0402c96a366fff216f7f199903f01ef1b4d1980b
Kenzie Togami <ket1999@gmail.com>
2014-02-17 18:34:02 -0800

    Transfer from EL

---
+++ src/elmodule/control/ControllerModule.java
@@ -0,0 +1,17 @@
+package elmodule.control;
+
+
+import emergencylanding.k.exst.modules.IModule;
+import emergencylanding.k.library.main.KMain;
+
+public class ControllerModule extends IModule {
+
+    public ControllerModule() {
+    }
+
+    @Override
+    public void init(KMain instance) {
+        System.err.println("Loaded Controllers via Mods interface.");
+    }
+
+}
