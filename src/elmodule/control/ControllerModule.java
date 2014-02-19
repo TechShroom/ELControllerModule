b7b0bd1a642386147471704a65ff435a3b424456
Kenzie Togami <ket1999@gmail.com>
2014-02-18 20:05:31 -0800

    Add check, fix error throwing breaking everything.

--- src/elmodule/control/ControllerModule.java
+++ src/elmodule/control/ControllerModule.java
@@ -1,14 +1,23 @@
 package elmodule.control;
 
+import k.core.util.classes.StackTraceInfo;
 import emergencylanding.k.exst.modules.IModule;
 import emergencylanding.k.library.main.KMain;
+import emergencylanding.k.library.util.LUtils;
 
 public class ControllerModule extends IModule {
     public static final String FULL_BINARY_NAME = ControllerModule.class
             .getName();
 
-    private ControllerModule() {
-        throw new AssertionError("Don't build this, get it from ModuleSystem!");
+    public ControllerModule() {
+        try {
+            LUtils.checkAccessor(
+                    new String[] { "sun.reflect.NativeConstructorAccessorImpl" },
+                    StackTraceInfo.getInvokingClassName());
+        } catch (Exception e) {
+            throw new IllegalAccessError(
+                    "Don't call controller module constructor");
+        }
     }
 
     @Override
